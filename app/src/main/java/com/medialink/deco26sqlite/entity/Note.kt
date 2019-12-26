3f078d06882f85e646239559bffd1b7ad6d952ed
Bambang <mail.buat.forum@gmail.com>
2019-12-26 16:20:01 +0700

    init

---
+++ app/src/main/java/com/medialink/deco26sqlite/entity/Note.kt
@@ -0,0 +1,12 @@
+package com.medialink.deco26sqlite.entity
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Note (
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var date: String? = null
+) : Parcelable
---
+++ app/src/main/java/com/medialink/deco26sqlite/helper/MappingHelper.kt
@@ -0,0 +1,21 @@
+package com.medialink.deco26sqlite.helper
+
+import android.database.Cursor
+import com.medialink.deco26sqlite.db.DatabaseContract
+import com.medialink.deco26sqlite.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(notesCursor: Cursor): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor.moveToFirst()
+        while (notesCursor.moveToNext()) {
+            val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            notesList.add(Note(id, title, description, date))
+        }
+
+        return notesList
+    }
+}
